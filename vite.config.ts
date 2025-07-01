c5c04d4f1883884a42336dc80bf62221b96ce826
Abhishek Raj <rajabhishek226@gmail.com>
2025-07-01 14:06:13 +0530

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  build: {
-    minify: false,
-  },
+  
   server: {
     host: "::",
     port: 8080,
