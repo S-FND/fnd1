d55a0e7881317c26478c103bc590e87556320043
Admin <admin@Admins-MacBook-Air.local>
2025-07-24 16:30:24 +0530

    solve entityId issue

--- src/features/enterprise-admin/components/profile/EditableCompanyProfile.tsx
+++ src/features/enterprise-admin/components/profile/EditableCompanyProfile.tsx
@@ -41,13 +41,10 @@ const EditableCompanyProfile = () => {
         setIsLoading(true);
         const data :any = await fetchProfileData();
         if (data) {
-          // Update user data in localStorage if user info exists in response
-          if (data) {
-            const rolePermissions = defaultPermissions[data.role] || {};
-            localStorage.setItem("fandoro-user", JSON.stringify(data));
-            setCurrentUser(data);
-          }
-          
+          const storedUser = JSON.parse(localStorage.getItem("fandoro-user") || "{}");
+          storedUser.entityId = data.entityId;
+          localStorage.setItem("fandoro-user", JSON.stringify(storedUser));
+          setCurrentUser(storedUser);
           setApiData(data);
           form.reset(data);
         }
